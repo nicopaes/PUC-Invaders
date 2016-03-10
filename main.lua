4ea1d978a275bc329ff4aaf88e278921bc8e2956
nikopaes <nickxp51@hotmail.com>
2016-03-09 22:07:41 -0300

    Adicionando script para o Logo RPG !

---
+++ main.lua
@@ -0,0 +1,23 @@
+local RPG_local = require "RPG_Full_Logo/RPG_Logo"
+
+local state
+
+function love.load()
+
+  
+
+  RPG_Logo.load(1.5,1.5,1.5)
+  state = RPG_Logo
+end
+function love.update(dt)
+  state.update(dt)
+end
+function love.draw()
+  state.draw()
+end
+function love.keypressed(key)
+  state.keypressed(key)
+end
+function love.gamepadpressed(joystick,button)
+  state.gamepadpressed(joystick,button)
+end
