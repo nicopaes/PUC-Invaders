0092c197d4f3bf719425dfc8da7e26e1b52118fa
Jose Eduardo Guimaraes Soares <je_soares94@yahoo.com.br>
2016-03-20 13:39:40 -0300

    fiz uma library de todas as funçoes uteis em player.lua do viking runner e passei pro future falls explicanado o q cada uma faz, nao cheguei a testar, foi mal, mas já deve ser uma boa ajuda aí pra gente...espero, foi mal pelo atraso btw

--- main.lua
+++ main.lua
@@ -3,17 +3,18 @@ local RPG_local = require "RPG_Full_Logo/RPG_Logo"
 local state
 
 function love.load()
-
-  
-
   RPG_Logo.load(1.5,1.5,1.5)
   state = RPG_Logo
+  player.load()
+  
 end
 function love.update(dt)
   state.update(dt)
+  player.update(dt)
 end
 function love.draw()
   state.draw()
+  player.draw()
 end
 function love.keypressed(key)
   state.keypressed(key)
