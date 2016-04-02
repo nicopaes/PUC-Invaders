6ed2be924deb44f461f370960aff10d506bbbe17
ruanlps <lopes.ruan@gmail.com>
2016-04-02 19:49:25 -0300

    Alterei o código para ficar mais modularizável + Aumentei a velocidade da nave para que ela consiga percorrer a tela toda verticalmente em, no máximo 2 segundos

--- main.lua
+++ main.lua
@@ -1,37 +1,39 @@
 debug = true  -- mudar pra false no release
 
+-------- BEGIN: Isso provavelmente deve virar uma classe em breve
+player = {
+          x = 200,
+          y = 710, 
+          speed = 400,
+          img = nil
+        }
 
-player = {x = 200, y = 710, speed = 150, img = nil}
+function player:move(dir)
+  newValue = self.x + dir
+  if 0 < newValue and newValue < love.graphics.getWidth() - player.img:getWidth() then
+    self.x = newValue
+  end
+end
+---------- END
 
-
-function love.load(arg)
+function love.load(arg) 
   player.img = love.graphics.newImage('assets/plane.png')
 end
+
 function love.update(dt)
-  --saindo do jogo quando o player apertar esc
-    if love.keyboard.isDown('escape') then
-      love.event.push('quit')
-    end
-    --movimentação lateral do player
-    if love.keyboard.isDown('left','a') then
-      --limitação lateral a esquerda dentro da área da janela
-      if player.x > 0 then
-      player.x = player.x - (player.speed*dt)
-    end
+  -- ESC: Sai do jogo
+  if love.keyboard.isDown('escape') then
+    love.event.push('quit')
+  -- Left Arrow ou A: Move para a esquerda
+  elseif love.keyboard.isDown('left','a') then
+    player:move(-player.speed*dt)
+  -- Right Arrow ou D: Move para a direita
   elseif love.keyboard.isDown('right','d') then
-    --limitação lateral a direita dentro da area da janela
-      --diminui a area total da janela pela area da img do player
-    if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
-      player.x = player.x + (player.speed*dt)
-    end
-    
+    player:move(player.speed*dt)
   end
-    
 end
 
 function love.draw(dt)
-  --Desenha a hit box do player (um retângulo no modo fill
-  --love.graphics.rectangle("fill",player.x,player.y,50,50)
-  --desenha o player
+  -- Desenha o player
   love.graphics.draw(player.img, player.x, player.y)
 end
