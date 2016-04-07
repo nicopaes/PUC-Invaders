ea5cc47b82875c5e996fe9ff7496bba8e8666fdb
ruanlps <lopes.ruan@gmail.com>
2016-04-07 09:46:32 -0300

    Criação de todos os inimigos + Gerenciamento deles como uma unidade

--- enemy.lua
+++ enemy.lua
@@ -3,7 +3,7 @@ require "shoot"
 Enemy = {
           x = 50,
           y = 50, 
-          speed = 0, -- Está alta, para faciolitar os testes (ideal +- 50)
+          speed = 20, -- Está alta, para faciolitar os testes (ideal +- 50)
           shotsFired = {},
           lastShoot = -1,
           rechargeTime = 500, -- Tempo para o próximo tiro, em milisegundos
@@ -35,30 +35,16 @@ function Enemy:draw()
   love.graphics.draw(self.img, self.x, self.y,0,self.scaleX,self.scaleY)
 end
 
-function Enemy:move(direction)
+function Enemy:move(dt)
   ---- Move o inimigo
-  -- Muda a direção dele caso ele tenha chegando em limites pré estabelecidos 
-  if self.hitWall == 0 then
-    newValue = self.x + direction
-  elseif self.hitWall == 1 then
-    newValue = self.x - direction
-  end
-
-  if self.x > 700 then
-    self.hitWall = 1
-    self.y = self.y + 50 --faz o inimigo mover-se para baixo, o ideal eh +-10, ta 50 pra testar mais rapido
-  end
   
-  if self.hitWall and self.x < 10 then
-    self.hitWall = 0 
-    self.y = self.y + 50 --faz o inimigo mover-se para baixo, o ideal eh +-10, ta 50 pra testar mais rapido
-  end
+  self.x = self.x + self.speed * dt
+end
 
-  -- Move apenas se não ultrapassar as bordas da tela e muda de direção
-  if 0 < newValue and newValue < love.graphics.getWidth() - self.img:getWidth()*self.scaleX  then
-    self.x = newValue  
-  end
+function Enemy:moveDown()
+  ---- Move inimido para baixo
   
+  self.y = self.y + 20
 end
 
 function Enemy:shoot()
--- level_manager.lua
+++ level_manager.lua
@@ -2,6 +2,8 @@ require "player"
 require "enemy"
 require "shoot"
 
+aliveEnemies = {}
+
 LevelManager = {
             level = 1
           }
@@ -20,48 +22,72 @@ function LevelManager:loadAgents()
   
   enemyList = {}
   for i = 1,5 do
-    enemy = Enemy:new({
-                       speed = 200
+    line = {}
+    for j = 1,8 do
+      enemy = Enemy:new({
+                       speed = 50 + 10*(self.level-1)
                       })
                       
-    enemy.x = 50 + (i-1) * (enemy.img:getWidth() * enemy.scaleX + 20)
-    
-    table.insert(enemyList, enemy)
+      enemy.x = 50 + (j-1) * (enemy.img:getWidth() * enemy.scaleX + 20)
+      enemy.y = 50 + (i-1) * (enemy.img:getHeight() * enemy.scaleY + 10)
+      
+      table.insert(line, enemy)
+      table.insert(aliveEnemies, enemy)
+    end
+    table.insert(enemyList, line)
   end
 end
 
 function LevelManager:moveEnemies(dt)
-  for i,enemy in ipairs(enemyList) do
-    enemy:move(enemy.speed*dt)
-    
-    enemy:shoot()
-    
-    if enemy.y > 710 then
-      love.graphics.print("GAME OVER", love.graphics.getWidth()/2,love.graphics.getHeight()/2)
+  for i,line in ipairs(enemyList) do
+    for j,enemy in ipairs(line) do 
+      enemy:move(dt)
+      
+      if enemy:right() >= love.graphics.getWidth() - 20 or
+        enemy:left() <= 20 then
+       
+          for k,line in ipairs(enemyList) do
+            for l,enemy in ipairs(line) do
+              enemy.speed = -enemy.speed
+              enemy:moveDown()
+            end
+          end
+      end
+      
+      if enemy.y > 710 then
+        love.graphics.print("GAME OVER", love.graphics.getWidth()/2, love.graphics.getHeight()/2)
+      end
     end
   end
+  
+  
 end
 
 function LevelManager:drawEnemies()
-  for i,enemy in ipairs(enemyList) do
-    enemy:draw()
-    enemy:drawShotsFired()
+  for i,line in ipairs(enemyList) do
+    for j,enemy in ipairs(line) do
+      enemy:draw()
+      enemy:drawShotsFired()
+    end
   end
 end
 
 function LevelManager:detectCollision()
   
   for i,s in ipairs(player.shotsFired) do
-    for j,enemy in ipairs(enemyList) do
-      if s:left() < enemy:right() and s:right() > enemy:left() and
-        s:top() < enemy:bottom() and s:bottom() > enemy:top() then
-          table.remove(player.shotsFired, i)
-          
-          table.remove(enemyList, j)
-          
-          if table.getn(enemyList) == 0 then
-            print "YOU WIN"
-          end
+    for j,line in ipairs(enemyList) do
+      for k,enemy in ipairs(line) do
+        
+        if s:left() < enemy:right() and s:right() > enemy:left() and
+          s:top() < enemy:bottom() and s:bottom() > enemy:top() then
+            table.remove(player.shotsFired, i)
+            
+            table.remove(line, k)
+            
+            if #aliveEnemies == 0 then
+              print "YOU WIN"
+            end
+        end
       end
     end
   end
